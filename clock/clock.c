1d975d583d9ab0b8fa262ba836c1d63e5c51088c
Dmitry Marakasov <amdmi3@amdmi3.ru>
2014-10-19 07:54:41 +0400

    Reuse variable

--- clock/clock.c
+++ clock/clock.c
@@ -8,21 +8,20 @@
 #include "twi.h"
 #include "panic.h"
 
-uint8_t zero = 0;
-uint8_t seconds = 0;
-
 int main(void) {
 	sleep_cpu();
 	ssd_init();
 
+	uint8_t val = 0;
+
 	twi_init();
-	if (twi_send_bytes(0b1101000, 0x0, &zero, 1) != 1)
+	if (twi_send_bytes(0b1101000, 0x0, &val, 1) != 1)
 		panic();
 
 	while(1) {
-		if (twi_recv_bytes(0b1101000, 0x0, &seconds, 1) != 1)
+		if (twi_recv_bytes(0b1101000, 0x0, &val, 1) != 1)
 			panic();
-		ssd_dec(seconds & 0x0f);
+		ssd_dec(val & 0x0f);
 	}
 
 	return 0;
